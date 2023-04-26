9be9fd80bace28cfa0b1af525332500ce0e9882f
Nguyen Huu Loc <103312138+Jackson1611@users.noreply.github.com>
2023-04-26 11:16:35 +0300

    skill route

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const app = express();
 const cors = require("cors");
 const morgan = require("morgan");
 const authRouter = require("./routes/auth");
+const skillRouter = require("./routes/skill");
 
 app.use(express.json());
 app.use(morgan("dev"));
@@ -15,8 +16,9 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true }).then(() => {
 });
 
 app.use("/api/auth", authRouter);
+app.use("/api/skill", skillRouter);
 
-port = 3000;
+let port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`app listening on port ${port}!`);
---
+++ server/routes/skill.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const router = express.Router();
+
+const Skill = require("../models/Skill");
+
+//@route POST api/skills
+//@desc Create a skill
+//@access private
+
+router.post("/", async (req, res) => {
+  const { title, description, url, status } = req.body;
+
+  if (!title) {
+    return res
+      .status(400)
+      .json({ success: false, msg: "Please enter a title" });
+  }
+
+  try {
+    const newSkill = new Skill({
+      title,
+      description,
+      url,
+      status: status || "to learn",
+      user: "64464dbb48503c0983720851",
+    });
+
+    await newSkill.save();
+    res.json({ success: true, msg: "Skill created", newSkill });
+  } catch (err) {}
+});
+
+module.exports = router;
