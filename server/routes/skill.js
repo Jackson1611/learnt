37096f4ad52138f9783807098de0d4a6005df554
Nguyen Huu Loc <103312138+Jackson1611@users.noreply.github.com>
2023-04-27 09:33:07 +0300

    CRUD completed

--- server/routes/skill.js
+++ server/routes/skill.js
@@ -17,6 +17,7 @@ router.get("/", verifyToken, async (req, res) => {
     res.status(500).json({ success: false, message: "Internal server error" });
   }
 });
+
 //@route POST api/skills
 //@desc Create a skill
 //@access private
@@ -42,6 +43,75 @@ router.post("/", verifyToken, async (req, res) => {
     await newSkill.save();
     res.json({ success: true, msg: "Skill created", newSkill });
   } catch (err) {}
+});
+
+//router PUT app/skills/:id
+//desc Update a skill
+//access private
+
+router.put("/:id", verifyToken, async (req, res) => {
+  const { title, description, url, status } = req.body;
+
+  // Simple validation
+  if (!title) {
+    return res
+      .status(400)
+      .json({ success: false, msg: "Please enter a title" });
+  }
+
+  try {
+    let updatedSkill = {
+      title,
+      description: description || "",
+      url: url || "",
+      status: status || "to learn",
+    };
+
+    const skillUpdateCondition = { _id: req.params.id, user: req.userId };
+
+    updatedSkill = await Skill.findOneAndUpdate(
+      skillUpdateCondition,
+      updatedSkill,
+      { new: true }
+    );
+
+    // User not authorised to update skill
+    if (!updatedSkill) {
+      return res.status(401).json({
+        success: false,
+        message: "Skill not found or user not authorised",
+      });
+    }
+
+    res.json({ success: true, message: "Updated skill", updatedSkill });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
+});
+
+//@route DELETE api/skills
+//@desc Delete a skill
+//@access private
+
+router.delete("/:id", verifyToken, async (req, res) => {
+  try {
+    const skillDeleteCondition = { _id: req.params.id, user: req.userId };
+    const deletedSkill = await Skill.findOneAndDelete(skillDeleteCondition);
+
+    // User not authorised to update skill
+    if (!deletedSkill) {
+      return res.status(401).json({
+        success: false,
+        message: "Skill not found or user not authorised",
+      });
+    }
+
+    res.json({ success: true, message: "Skill deleted" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
 });
 
 module.exports = router;
