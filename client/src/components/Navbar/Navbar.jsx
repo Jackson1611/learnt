c9a310deff79115540a2bae85e1569a77f8824bb
Nguyen Huu Loc <103312138+Jackson1611@users.noreply.github.com>
2023-05-07 10:32:31 +0300

    learning skill UI made

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -42,8 +42,8 @@ function Navbar({ user }) {
           <Button color="inherit" component={Link} to="/login">
             Login
           </Button>
-          <Button color="inherit" component={Link} to="/register">
-            Register
+          <Button color="inherit" component={Link} to="/skills/learning">
+            Learning
           </Button>
           <Button color="inherit" component={Link} to="/skills">
             Skills
